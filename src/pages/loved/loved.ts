1cbd6744e74bf4830a2d3a2c2499ef494eaeb3fe
Farsetti Luca <luca.farsetti@ad.lascaux.it>
2018-04-13 15:30:45 +0200

    Removed unused import

--- src/pages/loved/loved.ts
+++ src/pages/loved/loved.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CocktailsProvider } from '../../providers/cocktails/cocktails';
 import { Drink } from '../../models/drink';
-import { listItem } from '../../animations/animations';
 
 /**
  * Generated class for the LovedPage page.
